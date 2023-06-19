fdeef1bc49c379a7b2782ae98b1f58e13a65e055
PauloContrera <paulocontrera97@gmail.com>
2023-06-19 18:04:10 -0300

    hilos sin ejercicio 4

--- 09-hilos/hilos02.c
+++ 09-hilos/hilos02.c
@@ -1,41 +1,73 @@
 /*  
  * Ejercicio 2 del TP Hilos
- *
+Compile y ejecute el programa hilos02.c.
+- Analice el código. ¿Qué se debería ver por consola y qué es lo que se observa?
+- ¿Por qué no se ejecutará el printf de la línea 39?
+- Descomente la línea 30 y comente la 29 y vuelva a compilar. ¿Qué observa por
+ consola? ¿Por qué se ha modificado el comportamiento del programa?
  */
-
+ 
+// *Incluye las librerias, en especial pthread.h que es la libreria de hilos 
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+// *Variables Globales, t es del lazo for (No cambia donde sea declarada)
 int t = 0 ;
 int arre[5]={0};
 
+// *Define la funcion hola, que sera llamada por el hilo, donde en nro es el numero del hilo
 void *hola(void * nro) {
-   sleep(2);
+   //espera 2 segundos
+   sleep(2); 
+   // imprime el numero del hilo, enviado en el valor de la funcion *nro, 
+   // el nro lo fuerza a ser un entero, dado que esta declaro como void, xq c solo permite enviar void en hilos
    printf("Hola, soy el hilo %d\n", * (int*) nro);
+   // llama a pthread_exit(NULL), para finalizar el hilo
    pthread_exit(NULL);
 }
 
 int main() {
-pthread_t hilo[5];
-int rc;
+   pthread_t hilo[5];  // Se declara un arreglo de hilos
+   int rc;         // Se declaran variables para el código de retorno (No necesario)
 
+   // Crea un lazo for con la variable T (Global), para crear 5 hilos, estos hilos se ejercutan en paralelo
   for(t=0; t < 5 ; t++){
 
-     printf("Main creando el hilo nro %d\n", t);
-     arre[t] = t;
+   // Avisa que esta creando el hilo y guardando el arreglo en un vector
+   printf("Main creando el hilo nro %d\n", t);
+   arre[t] = t;
      
-     //rc = pthread_create(&hilo[t], NULL, hola , (void *)(&t)  );
+
+   // !Problematica, si usamos la linea 46 los hilos solo se crean en el ultimo valor de de t (=5)
+   // !En cambio si usamos la linea 47 los hilos se crean de 0 a 4 sin problema
+
+   //  rc = pthread_create(&hilo[t], NULL, hola , (void *)(&t)  );
      rc = pthread_create(&hilo[t], NULL, hola , (void *) (&arre[t]) );
      
+   /* 
+      !Explicando el error
+      El programa aún se compilará y ejecutará sin errores. 
+      Sin embargo, el comportamiento del programa se modificará. 
+      Ahora, todos los hilos imprimirán el número 5 en lugar de los valores del 0 al 4. 
+      Esto se debe a que la variable t en el bucle for se está pasando como argumento al hilo mediante el puntero &t
+      , pero la variable t sigue siendo modificada por el bucle antes de que cada hilo tenga la oportunidad de leer su valor. 
+      Como resultado, cuando los hilos se ejecutan y leen el valor de t, todos obtienen el valor final de t, que es 5.
+   */
+
+   // Hace un chequeo para saber que el hilo fue creado correctamente, =0 ok, >0 error  
      if (rc != 0){
          printf("ERROR; pthread_create() = %d\n", rc);
-         exit(-1);        }
+         exit(-1);
+      }
    }
-  
+
+   
+   // Eliminando el hilo main() y todos sus procesos posteriores
    pthread_exit(NULL);
 
+   // !Este proceso nunca se ejecuta, dado que pthread_exit(NULL); mato al proceso principal
    printf("Esto no aparece!!!\n");
    
 }
--- 09-hilos/hilos03.c
+++ 09-hilos/hilos03.c
@@ -1,48 +1,54 @@
 /*  
  * Ejercicio 3 del TP Hilos
- *   
+ Compile y ejecute el programa hilos03.c ¿Cuál es el objetivo del programa?.
+Observe la salida por consola.
+- Ahora, descomente la línea 19 y vuelva a compilar. ¿Qué observa por consola?
+¿Por qué se ha modificado el comportamiento del programa?
+- Ahora, comente la línea 61 y descomente la línea 62 y vuelva a compilar. Qué
+observa por consola? ¿Por qué se ha modificado el comportamiento del programa?
  */
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-pthread_t hilo[2];
+pthread_t hilo[2]; //declaran las variables globales hilo como un arreglo de hilos de tamaño 2
 
-void * hilo0()
+void * hilo0() // imprimen un mensaje en la consola y esperan 2 segundos antes de finalizar utilizando pthread_exit(NULL).
 {
-  //pthread_detach(pthread_self());      
+  //pthread_detach(pthread_self()); //Esta funcion hace que los el hilo no sea unible, por lo tanto hilo 1 no puede esperar a que termine hilo0,       
+  //pthread_self() devuelve el id del hilo actual, por eso al llamar a detach se necesita el id del hilo
   printf ("Yo soy el hilo 0 \n");
   sleep(2);  
   pthread_exit (NULL);
 }
 
-void * hilo1()
+void * hilo1() // imprimen un mensaje en la consola y esperan a que finalize hilo0, antes de finalizar utilizando pthread_exit(NULL).
 {
   printf ("Yo soy el hilo 1 \n");
   printf("Esperando al hilo %lx \n", hilo[0]);
   
-  pthread_join(hilo[0],NULL);
+  pthread_join(hilo[0],NULL); //Espera a que termine hilo0
   printf("Terminando hilo 1 \n");
   pthread_exit (NULL);
 }
 
 int main()
 {
-  int rc;
+  int rc; //almacena el código de retorno de las llamadas a pthread_create
   
-  //Crear hilo 0
+  //*Crear hilo 0
   rc = pthread_create (&hilo[0], NULL, hilo0, NULL);
-  if (rc)
+  if (rc) //Avisa si fallo
     {
       printf ("ERROR; pthread_create() = %d\n", rc);
       exit (-1);
     }    
   printf ("El main creando el hilo hola0, nro %lx\n",hilo[0]);
       
-  //Crear hilo 1
+  //*Crear hilo 1
   rc = pthread_create (&hilo[1], NULL, hilo1, NULL);  
-  if (rc)
+    if (rc) //Avisa si fallo
     {
       printf ("ERROR; pthread_create() = %d\n", rc);
       exit (-1);
@@ -55,3 +61,30 @@ int main()
   pthread_exit(NULL);
   //exit(0);
 }
+/*
+ Compile y ejecute el programa hilos03.c ¿Cuál es el objetivo del programa?.
+Observe la salida por consola.
+* El codigo base, ejecuta 2 hilos, 
+* hilo0 escribe por consola, luego en hilo1, espera a que termine hilo0 con join, 
+* imprime por consola
+* Y el main espera a que finalize el hilo1 (Que espera al hilo 0) para finalizar
+
+- Ahora, descomente la línea 19 y vuelva a compilar. ¿Qué observa por consola?
+¿Por qué se ha modificado el comportamiento del programa?
+* Al modificar el programa, lo que hacemos esque el hilo0 no sea unible, 
+* por lo tanto hilo1 no puede esperar a que hilo0 termine y hilo0 termina antes
+
+- Ahora, comente la línea 61 y descomente la línea 62 y vuelva a compilar. Qué
+observa por consola? ¿Por qué se ha modificado el comportamiento del programa?
+
+* No se mofica el comportamiento del programa, dado que pthread_exit(NULL); mata al hilo main(),
+* y si mata al hilo los procesos siguientes no se ejecutan 
+* exit(0); el proceso termina el programa main
+* Por lo tanto ambas maneras sirven para que el hilo main() termine
+
+! Importante, el programa no se modifica dado que el programa main espera a que termine el hilo1, pero si esta linea se comenta
+! El  pthread_exit(NULL) mata el hilo main, pero los demas hilos se siguen ejecutando (hilo1 termina despues que main)
+! El exit(0) mata todos los hilos ejecutandose y no deja terminar a hilo1
+
+~En consecuencia el uso correcto para terminar los main si se trabaja con hilo es pthread_exit(NULL);
+*/
